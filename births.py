5e30fd31f7f55bcf6f28c60f2038a2ecda76a64d
Sam Sieber <swsieber@gmail.com>
2014-06-02 08:03:13 -0600

    Changed from total people alive to percent alive (alive per 10,000) from the actuarial table.

--- births.py
+++ births.py
@@ -11,27 +11,37 @@ import sys
 import numpy as np
 from scipy.interpolate import interp1d
 
+verbose=False
 
 main_page_url = """http://www.ssa.gov/oact/NOTES/as120/LifeTables_Tbl_7.html"""
 test_table = 'http://www.ssa.gov/oact/NOTES/as120/LifeTables_Tbl_7_2100.html'
 
+def log(*args):
+	if verbose:
+		print args
+
 def get_table_urls(main_page_url):
 	url = main_page_url
 	url_base = "/".join(url.split("/")[:-1]) + "/"
+	log("looking up index", url)
 	soup = bs(urlopen(url))
-	year_options = soup.findAll('option')[1:]
+	year_options = soup.findAll('option')[1:12]
 	table_urls = [ url_base + str(page_option["value"]) for page_option in year_options]
 	table_years = [int(page_option.string) for page_option in year_options]
+	log("fetched list of urls to use")
 	return table_urls, table_years
 
 def get_table_data(table_url):
 	url = table_url
+	log("looking up table", url)
 	soup = bs(urlopen(url))
+	log("found table")
 	table = soup.find("table",border=1)
 	rows = table.findAll("tr", valign=None)
 	rows = [row for row,x in zip(rows,xrange(len(rows))) if x % 6 != 5 ]
-	important = [operator.itemgetter(0,5,12)(row.findAll("div")) for row in rows]
+	important = [operator.itemgetter(0,2,9)(row.findAll("div")) for row in rows]
 	data = map(lambda x: map(lambda y:float(y.string.replace(",","")),x),important)
+	log("found data")
 	return data
 
 def getAliveIn2014(year, table_data):
@@ -51,9 +61,11 @@ def interpolate_alives(data):
 	return interp1d(years,men)(points)	
 
 if __name__ == "__main__":
+	if len(sys.argv) > 0 and sys.argv[1] == "-v":
+		print "Setting verbose to be true"
+		verbose=True
 	urls, years = get_table_urls(main_page_url)
 	data = [get_table_data(url) for url in urls]
 	alives = [getAliveIn2014(year, datum) for year, datum in zip(years, data)]
-	#print alives
-	#data = [(1900, 0.0, 0.0), (1910, 38.0, 328.0), (1920, 8656.0, 30188.0), (1930, 152047.0, 293086.0), (1940, 635974.0, 918450.0), (1950, 1413720.0, 1800863.0), (1960, 2334716.0, 2775347.0), (1970, 3363298.0, 3812498.0), (1980, 4441973.0, 4884318.0), (1990, 5525378.0, 5956736.0), (2000, 6613985.0, 7029283.0), (2010, 7698786.0, 8097976.0), (2020, 0, 0), (2030, 0, 0), (2040, 0, 0), (2050, 0, 0), (2060, 0, 0), (2070, 0, 0), (2080, 0, 0), (2090, 0, 0), (2100, 0, 0)]
-	alive_birth_distribution = interpolate_alives(data)
+	log(alives)
+	alive_birth_distribution = interpolate_alives(alives)
